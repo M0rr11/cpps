285e5bd4af03bd1b983e8037772cdfb1e4e5f200
Aya Hakimi <ayhakimi@e1r2p10.1337.ma>
2026-04-14 15:12:08 +0100

    08

---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    try
+    {
+        std::vector<int>::iterator it = easyfind(v, 3);
+        std::cout << *it << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
